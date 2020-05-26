002be82478feb022ed9feaa9885b93ea5cf22278
Johnny Li <reptile18@gmail.com>
2020-05-25 17:32:57 -0700

    basic end-to-end functionaity working

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,31 @@
-const questions = [
+// imports
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const genMD = require("./utils/generateMarkdown.js");
 
+// variables, objects
+const questions = [
+  {
+    message: "Title: ",
+    name: "title"
+  },
+  {
+    message: "Description: ",
+    name: "description"
+  }
 ];
+const writeFileAsync = util.promisify(fs.writeFile);
 
 function writeToFile(fileName, data) {
+  const markdownRaw = genMD(data);
+
+  writeFileAsync(fileName,markdownRaw);
 }
 
-function init() {
-
+async function init() {
+  let data = await inquirer.prompt(questions);
+  await writeToFile("README.md",data);
 }
 
 init();
